b9585988af9b34b56371902b5ef77c315b9df4fe
Victor Anibal <153691103+victor-anibal@users.noreply.github.com>
2024-01-19 23:44:18 -0300

    Update Jogo_de _adivinhação.py

--- Python-Project/Jogo_de _adivinhação.py
+++ Python-Project/Jogo_de _adivinhação.py
@@ -3,23 +3,50 @@ print(8*"*//*")
 print("Bem-vindo ao jogo de adivinhação!")
 print(8*"*//*")
 
-numeroMax = 100
-numero_secreto = (random.randrange(1, numeroMax + 1))
+
+
+print("(1) Fácil\n(2) Médio\n(3) Difícil")
+
+nivel = 0
+
+while nivel <= 0 or nivel > 3:
+    nivel = int(input("Defina um nível: "))
+    print("Escolha um número entre 1 e 3!")
+
+
+pontos = 1000
+total_tentativa = 0
+
+if nivel == 1:
+    numero_max = 100
+    total_tentativa = 20
+
+elif nivel == 2:
+    numero_max = 500
+    total_tentativa = 15
+
+else:
+    numero_max = 1000
+    total_tentativa = 10
+
+numero_secreto = (random.randrange(1,  numero_max + 1))
+
+
 print(numero_secreto)
-total_tentativa = 3
+
 rodada = 1
 chute = -1
 tentativa = total_tentativa
-print("Neste jogo você tem até 3 tentativas para acertar o número secreto!!!")
+print(f"Neste jogo você tem até {total_tentativa} tentativas para acertar o número secreto!!!")
 
 
 for rodada in range(1, total_tentativa + 1):
 
-    print(f"Tentativa {rodada} de 3")
-    chute = int(input(f"Digite um número entre 1 e {numeroMax}: "))
+    print(f"Tentativa {rodada} de {total_tentativa}")
+    chute = int(input(f"Digite um número entre 1 e {numero_max}: "))
     tentativa = tentativa - 1
-    if chute < 1 or chute > 100:
-        print(f'Você deve digitar um número entre 1 e {numeroMax}!')
+    if chute < 1 or chute > numero_max:
+        print(f'Você deve digitar um número entre 1 e {numero_max}!')
         continue
 
 
@@ -35,6 +62,8 @@ for rodada in range(1, total_tentativa + 1):
             print(f"O número secreto é maior que {chute}")
         elif maior:
             print(f"O número secreto é menor que {chute}")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
     if tentativa == 0 and chute != numero_secreto:
         print(" Seu número de tentativas acabou!\n Mas ainda não é o fim.\n Tente novamente e ganhe na próxima vez!")
 
